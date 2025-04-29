72d462d82776ca308cacdd97dbcfe812e61d83c6
erlopez11 <lopezelainy@gmail.com>
2025-04-28 20:42:13 -0500

    wip:setup postgresql db & edition model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+TECHNIQUES = (
+    ('sp', 'Screenprint'),
+    ('mt', 'Monotype'),
+    ('li', 'Lithography'),
+    ('co', 'Collagraph'),
+    ('dp', 'Drypoint'),
+    ('at', 'Aquatint'),
+    ('mt', 'Mezzotint'),
+    ('et', 'Etching'),
+    ('eg', 'Engraving'),
+    ('pg', 'Photogravure'),
+    ('lc', 'Linocut'),
+    ('wc', 'Woodcut'),
+    ('mk', 'Mokuhanga'),
+    ('cb', 'Carborundum')
+)
+
+STATUS = (
+    ('ip', 'In Progress'),
+    ('c', 'Complete'),
+    ('a', 'Archive')
+)
+
+EDITION_TYPE = (
+    ('lmt', 'Limited'),
+    ('op', 'Open'),
+    ('va', 'Varied')
+
+)
+
+class Edition(models.Model):
+    edition_name = models.CharField(max_length=200)
+    year = models.PositiveIntegerField(validators=[MinValueValidator(1900)])
+    edition_size = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    paper_size = models.CharField(max_length=100)
+    plate_size = models.CharField(max_length=100)
+    technique = models.CharField(choices=TECHNIQUES, default=[0][0])
+    margin_upper = models.CharField(max_length=100)
+    margin_lower = models.CharField(max_length=100)
+    margin_sides = models.CharField(max_length=100)
+    available_prints = models.CharField(max_length=50)
+    status = models.CharField(choices=[0][0])
+    edition_type = models.CharField(choices=[0][0])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.edition_name
+    
+
