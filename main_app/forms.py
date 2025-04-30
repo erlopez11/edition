8111832f7d1e81d0cbec46b4d6663f1422160f8e
erlopez11 <lopezelainy@gmail.com>
2025-04-29 19:24:49 -0500

    wip:create note form template

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,25 @@
+from django import forms
 from django.forms import ModelForm
 from cloudinary.forms import CloudinaryFileField
-from .models import Edition
+from .models import Edition, Note
 
 class EditionForm(ModelForm):
     image = CloudinaryFileField()
     class Meta:
         model = Edition
         fields = ['edition_name', 'image', 'year', 'edition_size', 'paper_size', 'plate_size', 'technique', 'margin_upper', 'margin_lower', 'margin_sides', 'available_prints', 'status', 'edition_type']
+
+class NoteForm(ModelForm):
+    class Meta:
+        model = Note
+        fields = ['note_date', 'notes']
+
+        widgets = {
+            'note_date': forms.DateInput(
+                format=('%y-%m-%d'),
+                attrs={
+                    'placeholder': 'Select a date',
+                    'type': 'date'
+                }
+            ),
+        }
