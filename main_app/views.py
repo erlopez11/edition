eb7d09547c42898dbd0ea6af10e4ec5dd133f793
erlopez11 <lopezelainy@gmail.com>
2025-05-06 12:59:23 -0500

    fix:typo in signup

--- main_app/views.py
+++ main_app/views.py
@@ -25,7 +25,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('edition_index')
+            return redirect('editions_index')
         else:
             error_message = 'Invalid sign up please try again'
     form = UserCreationForm()
