c51a7a606271b3d2e9c9da907e1ebfcc5dbeef40
erlopez11 <lopezelainy@gmail.com>
2025-05-05 20:09:15 -0500

    fix: api key not accessed by heroku

--- edition/settings.py
+++ edition/settings.py
@@ -29,8 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+if not 'ON_HEROKU' in os.environ:
+    DEBUG = True
 
-DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
